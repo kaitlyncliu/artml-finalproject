57defbb8446fc3f73baa801b0f4834856894e21d
Kaitlyn Liu <liu.kaitlync@gmail.com>
2024-04-18 22:50:06 -0400

    add font and fix routing

--- lookingGlass/lookingGlass/urls.py
+++ lookingGlass/lookingGlass/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("lookingGlassApp/", include("lookingGlassApp.urls")),
+    path("", include("lookingGlassApp.urls")),
 ]
